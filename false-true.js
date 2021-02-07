9f6e0ceeebfa76d407a3275c88d07c089c24fad1
Abdullah Al Mamun <aamamun9391@gmail.com>
2021-02-08 00:02:41 +0600

    added true false

---
+++ false-true.js
@@ -0,0 +1,22 @@
+//falsy 
+//false;
+//0;
+//NaN;
+//"";
+//undifind;
+//null;
+
+//truthy
+//" ";
+//'0';
+//[]
+
+
+let num=[];
+if(num){
+    console.log("condition is true")
+
+}
+else{
+    console.log("condition is false")
+}
