2209bab4bfe68d6be86f4737866a44ab7062d2f9
Abdullah Al Mamun <aamamun9391@gmail.com>
2021-02-08 02:10:22 +0600

    added

---
+++ clourse.js
@@ -0,0 +1,20 @@
+function stopWatch(){
+    count=0;
+    return function(){
+        return count++;
+    }
+}
+
+const clock1 = stopWatch();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+
+
+const clock2=stopWatch();
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
