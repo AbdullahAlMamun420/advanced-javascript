4606b85789fa27f4c6bf9ae4a117d8c1faf6359b
Abdullah Al Mamun <aamamun9391@gmail.com>
2021-02-08 02:34:14 +0600

    added

--- array-split.js
+++ array-split.js
@@ -2,7 +2,7 @@ const nums=[5,32,56,3,43,55,45,65];
 
 // const result=nums.slice(2,5)//index 2 thake index5 er age porjonto sorabe but array poriborton korbena
 
-const result=nums.splice(2,4)
+const result=nums.splice(2,4,33,54)//2 thake 4 ta element bad dibe ,array poriborton korbe 33 54 add korbe
 console.log(result)
 console.log(nums)
 
