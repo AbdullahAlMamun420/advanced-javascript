b44011a493c8ec7a9e5835629581d600d4a5ff32
Abdullah Al Mamun <aamamun9391@gmail.com>
2021-02-08 02:23:02 +0600

    added array split

---
+++ array-split.js
@@ -0,0 +1,11 @@
+const nums=[5,32,56,3,43,55,45,65];
+
+// const result=nums.slice(2,5)//index 2 thake index5 er age porjonto sorabe but array poriborton korbena
+
+const result=nums.splice(2,4)
+console.log(result)
+console.log(nums)
+
+const arrayjoin=nums.join(" ")
+
+console.log(arrayjoin)
