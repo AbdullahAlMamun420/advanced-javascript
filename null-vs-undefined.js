88e2c7e313effefa69d212ff382d3e97f1e06d3e
Abdullah Al Mamun <aamamun9391@gmail.com>
2021-02-08 00:31:18 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let num;
+// console.log(num);
+
+
+// function number(num1,num2){
+//     const total =num1+num2;
+//     return 
+// }
+// console.log(number(10,12))
+// console.log(number(10))
+
+// const parmik={name:"priya",phone:3353664,address:"dhaka"}
+// console.log(parmik.gf)
+
+// const array=[12,33,44,34];
+// console.log(array[44])
+
+let value=null;
